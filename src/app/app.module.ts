6dcd3a03d9c9620495a5b69e11f8eecf8ea9a7d9
nvdinh185 <nvdinh0905671805@gmail.com>
2019-09-05 10:03:24 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ParrentComponent } from './parrent.component';
-import { ChildComponent } from './child.component';
+import { CardComponent } from './card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParrentComponent,
-    ChildComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
